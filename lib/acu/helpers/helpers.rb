6d818e0fed1368ef2099e14088f6f5260c74e4e1
dariush <b.g.dariush@gmail.com>
2018-08-20 18:23:26 +0430

    @ lib/acu/helpers/helpers.rb: negated symbols supported

--- lib/acu/helpers/helpers.rb
+++ lib/acu/helpers/helpers.rb
@@ -1,6 +1,12 @@
 def acu_is? symbol
   flag = false
-  [symbol].flatten.each { |s| flag |= Acu::Monitor.valid_for? s }
+  [symbol].flatten.each do |s| 
+  	if s.to_s =~ /\Anot_/
+			flag |= not(Acu::Monitor.valid_for?(s.to_s.gsub(/\Anot_/, "").to_sym))
+		else
+			flag |= Acu::Monitor.valid_for? s 
+		end
+  end
   flag
 end
 
